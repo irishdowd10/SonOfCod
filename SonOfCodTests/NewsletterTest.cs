c0b57ecace8e34b34216186752b3bb06ba52d46a
John Dowd <irishdowd10@gmail.com>
2017-08-12 13:22:25 -0700

    tried to run first test ---- no luck.  Add content to About page

---
+++ SonOfCodTests/NewsletterTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SonOfCod.Models;
+using Xunit;
+
+namespace SonOfCodTests
+{
+    class NewsletterTest
+    {
+		[Fact]
+		public void GetNameTest()
+		{
+			//Arrange
+			var newsletter = new Newsletter();
+
+			//Act
+			var result = newsletter.Name;
+
+			//Assert
+			Assert.Equal("John", result);
+		}
+	}
+}
