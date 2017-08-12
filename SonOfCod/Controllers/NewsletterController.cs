002fd98cca3097d9426779d6ba2314486431e80f
John Dowd <irishdowd10@gmail.com>
2017-08-11 17:11:45 -0700

    having problems wiht db to save to newsletetr db --- Parrelles haveing issues agin

---
+++ SonOfCod/Controllers/NewsletterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCod.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SonOfCod.Controllers
+{
+	public class NewsletterController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
+		//Create your newletter
+		[Authorize]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[Authorize]
+		[HttpPost]
+		public IActionResult Create(Newsletter newsletter)
+		{
+			db.Newsletters.Add(newsletter);
+			db.SaveChanges();
+			ViewBag.addedNewsletterName = db.Newsletters.Last().Name;
+			ViewBag.addedNewsletterEmail = db.Newsletters.Last().Email;
+			return View();
+		}
+	}
+}
